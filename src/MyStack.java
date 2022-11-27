b04dd9cd82d173b0e1f501030766f47aacdc22aa
Kevin <kevinsbarski@gmail.com>
2022-11-27 16:23:01 +0200

    Stack project

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class MyStack {
@@ -29,31 +30,75 @@ public class MyStack {
              int copyStack [] = new int[stack.length+1];
              copyStack[0] = num;
              flag+=1;
-             for(int i = 1; i < copyStack.length-1; i++){
-                copyStack[i] = stack[i-1];
-                flag+=1;
+             for(int i = 1; i < copyStack.length; i++){
+                copyStack[i] = this.stack[i-1];
              }
-
-             copyStack[flag-1] = num;
-             stack = copyStack;
+             this.stack = copyStack;
         }
 
     }
-    public void pop(){
-        if (flag <= 0)
-            throw new ArrayIndexOutOfBoundsException("Cannot pop! Stack is empty.");
+    public int pop()throws EmptyStackException{
+        int popedNumber;
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        else{
+            System.out.println("before pop stack flag="+flag);
+            System.out.println(stack.length);
+            flag-=1;
+            popedNumber = this.stack[0];
+            int copyStack [] = new int[stack.length-1];
+            for(int i = 0; i < copyStack.length; i++){
+                copyStack[i] = stack[i+ 1];
+            }
+            stack = copyStack;
+            System.out.println("after pop func flag="+flag);
 
-
+            }
+        return popedNumber;
     }
+
+    public int peek()throws Exception {
+
+            if (this.isEmpty()) {
+                throw new EmptyStackException();
+            } else {
+                return this.stack[0];
+            }
+    }
+
+
+
+
 
     public int[] getStack() {
         for(int i=0; i<stack.length; i++){
-            System.out.print("the elemnet is"+stack[i]);
+            System.out.print("the elemnet is "+stack[i]);
+
         }
+        System.out.println();
         return stack;
     }
 
     public int getFlag() {
         return flag;
+    }
+//    public String ToString(){
+//
+//
+//    }
+
+    @Override
+    public String toString()throws EmptyStackException {
+        if(this.isEmpty())
+            throw new EmptyStackException();
+
+        String str = "[";
+        for (int i = 0; i < this.stack.length-1; i++) {
+            str += this.stack[i] + ", ";
+        }
+        str += this.stack[this.stack.length-1];
+        str += "]";
+        return str;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,32 @@
+import java.util.EmptyStackException;
+
 public class main {
-    public static void main(String [] args){
+    public static void main(String [] args)throws Exception{
         MyStack stack = new MyStack();
+        System.out.println("the stack is empty?"+stack.isEmpty());
         stack.push(3);
+        System.out.println(stack.getStack());
+        System.out.println("the stack is empty?"+stack.isEmpty());
         stack.push(4);
         System.out.println(stack.getStack());
+        stack.push(6);
+        System.out.println(stack.getStack());
+        System.out.println(stack.toString());
+        stack.pop();
+        System.out.println("first popo was succeses");
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.getStack());
+        System.out.println("the stack is empty?"+stack.isEmpty());
+        System.out.println(stack.getStack());
+        System.out.println(stack.getFlag());
+        try{
+            System.out.println(stack.peek());
+        }catch (EmptyStackException e){
+            System.out.println(e.toString());
+        }
+
         System.out.println("end");
     }
+
 }
